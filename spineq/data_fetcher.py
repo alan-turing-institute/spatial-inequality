60fa40c77425f20520aec935b9afc274d2d580c4
Jack Roberts <jroberts@turing.ac.uk>
2020-02-25 16:41:52 +0000

    refactoring data download/processing

--- spineq/data_fetcher.py
+++ spineq/data_fetcher.py
@@ -3,6 +3,8 @@ from io import BytesIO
 import zipfile
 import time
 
+import warnings
+
 import requests
 
 import pandas as pd
@@ -40,21 +42,28 @@ def load_gdf(path, epsg=27700):
     return gdf
 
 
-def get_la():
+def download_la(overwrite=False):
     save_path = RAW_DIR + "/la/la.shp"
+    if os.path.exists(save_path) and not overwrite:
+        return gpd.read_file(save_path)
     url = "https://ons-inspire.esriuk.com/arcgis/rest/services/Administrative_Boundaries/Local_Athority_Districts_December_2018_Boundaries_GB_BFC/MapServer/0/query?where=UPPER(lad18nm)%20like%20'%25NEWCASTLE%20UPON%20TYNE%25'&outFields=*&outSR=27700&f=json"
     return query_ons_records(url, save_path=save_path)
 
 
-def get_oa():
+def download_oa(overwrite=False):
     save_path = RAW_DIR + "/oa/oa.shp"
+    if os.path.exists(save_path) and not overwrite:
+        return gpd.read_file(save_path)
     url = "https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/OA_DEC_2011_EW_BFC/FeatureServer/0/query?where=1%3D1&outFields=*&geometry=-2.160%2C54.936%2C-1.052%2C55.074&geometryType=esriGeometryEnvelope&inSR=4326&spatialRel=esriSpatialRelIntersects&outSR=27700&f=json"
 
     return query_ons_records(url, save_path=save_path)
 
 
-def get_centroids():
-    save_path = RAW_DIR + "/centroids/centroids.shp"
+def download_centroids(overwrite=False):
+    save_path = RAW_DIR + "/centroids.csv"
+    if os.path.exists(save_path) and not overwrite:
+        return pd.read_csv(save_path)
+    
     url = "https://ons-inspire.esriuk.com/arcgis/rest/services/Census_Boundaries/Output_Area_December_2011_Centroids/MapServer/0/query?where=1%3D1&outFields=*&geometry=-2.177%2C54.917%2C-1.069%2C55.055&geometryType=esriGeometryEnvelope&inSR=4326&spatialRel=esriSpatialRelIntersects&outSR=27700&f=json"
     centroids = query_ons_records(url, save_path=save_path)
     centroids["X"] = centroids["geometry"].x
@@ -63,10 +72,12 @@ def get_centroids():
     return centroids
 
 
-def get_populations(overwrite=False):
-    save_path = RAW_DIR + "/populations.csv"
-    if os.path.exists(save_path) and not overwrite:
-        return pd.read_csv(save_path)
+def download_populations(overwrite=False):
+    save_path_total = RAW_DIR + "/population_total.csv"
+    save_path_ages = RAW_DIR + "/population_ages.csv"
+    if (os.path.exists(save_path_total) and os.path.exists(save_path_ages)
+        and not overwrite):
+        return pd.read_csv(save_path_total), pd.read_csv(save_path_ages)
     
     url = "https://www.ons.gov.uk/file?uri=%2fpeoplepopulationandcommunity%2fpopulationandmigration%2fpopulationestimates%2fdatasets%2fcensusoutputareaestimatesinthenortheastregionofengland%2fmid2018sape21dt10d/sape21dt10dmid2018northeast.zip"
     r = requests.get(url)
@@ -87,50 +98,46 @@ def get_populations(overwrite=False):
     df = pd.read_excel(xl_file,
                        sheet_name="Mid-2018 Persons",
                        skiprows=4,
-                       usecols=("OA11CD", "LSOA11CD", "All Ages"))
+                       thousands=",")
 
-    df.rename(columns={"All Ages": "Population"},
-              inplace=True)
+    df_total = df[["OA11CD", "All Ages"]]
+    df_total.rename(columns={"All Ages": "Population"}, inplace=True)
+    df_total.to_csv(save_path_total, index=False)
+
+    df_ages = df.drop("All Ages", axis=1)
+    df_ages.to_csv(save_path_ages, index=False)
     
-    df.to_csv(save_path, index=False)
-    
-    return df
+    return df_total, df_ages
 
 
-def make_data_files():
+def download_workplace(overwrite=False):
+    save_path = RAW_DIR + "/workplace.csv"
+    if overwrite:
+        warnings.warn("Not possible to download workplace data directly. Go to https://www.nomisweb.co.uk/query/construct/summary.asp?reset=yes&mode=construct&dataset=1228&version=0&anal=1&initsel=")
+
+    return pd.read_csv(save_path, thousands=',')
+
+
+def download_data_files(overwrite=False):
     print("LOCAL AUTHORITY BOUNDARIES")
-    la = get_la()
+    la = download_la(overwrite=overwrite)
+    print(la.head())
     
     print("OUTPUT AREA BOUNDARIES")
-    oa = get_oa()
-
+    oa = download_oa(overwrite=overwrite)
+    print(oa.head())
+    
     print("OUTPUT AREA CENTROIDS")
-    centroids = get_centroids()
+    centroids = download_centroids(overwrite=overwrite)
+    print(centroids.head())
     
     print("OUTPUT AREA POPULATIONS")
-    populations = get_populations()
+    population_total, population_ages = download_populations(overwrite=overwrite)
+    print(population_total.head())
     
-    print("MERGING")
-    oa = oa[oa.intersects(la["geometry"])]
-    
-    print(centroids.head())
-    centroids = centroids.merge(populations,
-                                left_on='oa11cd',
-                                right_on='OA11CD')
-
-    oa = oa.merge(centroids[["X", "Y", "oa11cd", "Population"]],
-                  how="left", on="oa11cd")
-
-    print("SAVING")
-    save_dir = DATA_DIR + "/processed"
-    os.makedirs(save_dir, exist_ok=True)
-    
-    select_cols = ["oa11cd", "lad11cd", "geometry", "X", "Y", "Population"]
-    oa[select_cols].to_file(save_dir + "/processed.shp")
-    
-    print("DONE!")
-    
-    return oa
+    print("WORKPLACE")
+    workplace = download_workplace(overwrite=overwrite)
+    print(workplace.head())
 
 
 def query_ons_records(base_query, time_between_queries=1,
@@ -208,3 +215,49 @@ def query_ons_records(base_query, time_between_queries=1,
         all_records.to_file(save_path)
 
     return all_records
+
+
+def get_oa_stats():
+    data = {"population_ages": population_ages,
+            "workplace": workplace}
+    
+    population_ages = data["population_ages"]
+    workplace = data["workplace"]
+
+
+def process_data_files():
+    la = download_la(overwrite=False)
+    oa = download_oa(overwrite=False)
+    centroids = download_centroids(overwrite=False)
+    workplace = download_workplace(overwrite=False)
+    population_total, population_ages = download_populations(overwrite=False)
+    
+    # OAs to keep: those that intersect LA geometry
+    oa.columns = oa.columns.str.lower()
+    oa = oa[oa.intersects(la["geometry"])]
+    oa = oa[["oa11cd", "geometry"]]
+    oa.to_file(PROCESSED_DIR + "/oa_shapes.shp")
+    oa_to_keep = oa["oa11cd"].values
+    
+    # filter centroids
+    centroids.columns = centroids.columns.str.lower()
+    centroids = centroids[centroids["oa11cd"].isin(oa_to_keep)]
+    centroids = centroids[["oa11cd", "X", "Y"]]
+    centroids.to_csv(PROCESSED_DIR + "/centroids.csv", index=False)
+    
+    # filter population data
+    population_total.columns = population_total.columns.str.lower()
+    population_total = population_total[population_total["oa11cd"].isin(oa_to_keep)]
+    population_total = population_total[["oa11cd", "population"]]
+    population_total.to_csv(PROCESSED_DIR + "/population_total.csv", index=False)
+    
+    population_ages.columns = population_ages.columns.str.lower()
+    population_ages = population_ages[population_ages["oa11cd"].isin(oa_to_keep)]
+    population_ages.to_csv(PROCESSED_DIR + "/population_ages.csv", index=False)
+        
+    # filter worokplace
+    workplace.columns = workplace.columns.str.lower()
+    workplace = workplace[workplace["oa11cd"].isin(oa_to_keep)]
+    workplace.to_csv(PROCESSED_DIR + "/centroids.csv", index=False)
+
+
