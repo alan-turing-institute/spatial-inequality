587abcd5e80ddaa35072ad6e366bf7623ad0b034
jack89roberts <jroberts@turing.ac.uk>
2021-04-20 13:55:29 +0100

    fix dependency versions and socketio client

--- scripts/client.py
+++ scripts/client.py
@@ -2,52 +2,54 @@
 Demonstration of how to interact with the API via WebSockets
 """
 
-from socketIO_client import SocketIO, LoggingNamespace
+import time
+import socketio
 
 JOB_ID = None
 
+# Define host and callbacks
+sio = socketio.Client()
 
+
+@sio.on("message")
 def print_message(message):
     print("MESSAGE", message)
 
 
+@sio.on("job")
 def print_job(job):
     print("JOB", job)
 
 
+@sio.on("queue")
 def print_queue(queue):
     print("QUEUE", queue)
 
 
+@sio.on("jobFinished")
 def job_finished(data):
     global JOB_ID
     JOB_ID = data["job_id"]
     print("JOB FINISHED", data)
 
 
+@sio.on("jobProgress")
 def print_progress(progress):
     print("PROGRESS", progress)
 
 
-# Define host and callbacks
-# socketIO = SocketIO('localhost', 5000)
-socketIO = SocketIO("optimisation-backend.azurewebsites.net", 80)
-socketIO.on("message", print_message)
-socketIO.on("job", print_job)
-socketIO.on("queue", print_queue)
-socketIO.on("jobProgress", print_progress)
-socketIO.on("jobFinished", job_finished)
-
 # Make connection
 print("CONNECT")
-socketIO.emit("connect")
-socketIO.wait(seconds=1)
+#sio.connect("https://optimisation-backend.azurewebsites.net")
+sio.connect("http://localhost:5000")
+
+time.sleep(1)
 print("----------")
 
 # Submit an optimisation job: client emits submitJob, server responds by
 # emitting job
 print("SUBMIT JOB")
-socketIO.emit(
+sio.emit(
     "submitJob",
     {
         "n_sensors": 3,
@@ -58,40 +60,34 @@ socketIO.emit(
         "workplace_weight": 0.5,
     },
 )
-socketIO.wait(seconds=1)
+time.sleep(1)
 print("----------")
 
-print("WAITING")
-socketIO.wait(seconds=20)
+print("WAITING 1 MINUTE FOR JOB PROGRESS")
+time.sleep(60)
 # should see a jobFinished message once the job has completed
 print("----------")
-
 
 # Get job result/status: client emits "getJob", server responds by emitting
 # "job" event
 print("GET JOB")
-socketIO.emit("getJob", JOB_ID)
-socketIO.wait(seconds=1)
+sio.emit("getJob", JOB_ID)
+time.sleep(1)
 print("----------")
 
 # List jobs on the queue and their status: client emits "getQueue", server
 # responds by emitting "queue" event
 print("LIST QUEUE")
-socketIO.emit("getQueue")
-socketIO.wait(seconds=1)
+sio.emit("getQueue")
+time.sleep(1)
 print("----------")
 
 # Delete a job: client emits "deleteJob", server responds by emitting "message"
 # event
 print("DELETE JOB")
-socketIO.emit("deleteJob", JOB_ID)
-socketIO.wait(seconds=2)
+sio.emit("deleteJob", JOB_ID)
+time.sleep(1)
 print("----------")
 
-
-# Delete everything in the queue: client emits "deleteQueue", server responds
-# by emitting "message" event
-print("DELETE QUEUE")
-socketIO.emit("deleteQueue")
-socketIO.wait(seconds=2)
-print("----------")
+print("DISCONNECT")
+sio.disconnect()
